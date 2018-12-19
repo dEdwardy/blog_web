7ee1d7b463cdce69c2668804f8e93ab45d074846
957196958@qq.com <957196958@qq.com>
2018-12-19 17:07:08 +0800

    '2018-12-09'

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mpr-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'mpr';
+}
