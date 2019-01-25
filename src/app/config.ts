d96a87bd8b98661fc2266c63b59ff83e18f7d03f
Edward <957196958@qq.com>
2019-01-25 14:18:24 +0800

    Update config.ts

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,5 +1,4 @@
-// export const baseUrl='http://106.12.202.20:8088/api';
-// export const pathHead ='http://106.12.202.20:8088';  //node静态服务器地址
+
 
 export const baseUrl='http://172.16.4.78:8088/api';
 export const pathHead ='http://172.16.4.78:8088';  //node静态服务器地址
