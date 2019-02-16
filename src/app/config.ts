f3d324ae0f54c1bcdecd79c86441f5ad28771ec3
dEdwardy <957196958@qq.com>
2019-02-16 23:43:54 +0800

    1

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-export const baseUrl='http://106.12.202.20:8088/api';
-export const pathHead ='http://106.12.202.20:8088';  //node静态服务器地址
-
-// export const baseUrl='http://172.16.4.78:8088/api';
-// export const pathHead ='http://172.16.4.78:8088';  //node静态服务器地址
-
-// export const baseUrl='http://localhost:8088/api';
-// export const pathHead ='http://localhost:8088';  //node静态服务器地址
-
-
-
-// export const baseUrl= '/api';
-// export const pathHead= 'http://172.16.4.78:8088';
-=======
 
 // export const baseUrl='http://localhost:8088/api';
 // export const pathHead ='http://localhost:8088';  //node静态服务器地址
@@ -23,4 +8,3 @@ export const pathHead ='http://106.12.202.20:8088';  //node静态服务器地址
 
 export const baseUrl= 'http://106.12.202.20:8088/api';
 export const pathHead= 'http://106.12.202.20:8088';
->>>>>>> 1b69821255483c2fe0c5ceea5477466b9f647cdd
