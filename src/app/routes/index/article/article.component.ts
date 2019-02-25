af72157204472d34526767bef757b0da451ae94b
dEdwardy <957196958@qq.com>
2019-02-25 19:25:08 +0800

    修复dom事件重复bind

--- src/app/routes/index/article/article.component.ts
+++ src/app/routes/index/article/article.component.ts
@@ -23,7 +23,11 @@ export class ArticleComponent implements OnInit {
   pageNumber: number;
   search:Boolean = false;
   currentPage:number = 1;
-  constructor(public articleService:ArtilceService, public router:Router, private route:ActivatedRoute) { }
+  constructor(public articleService:ArtilceService, public router:Router, private route:ActivatedRoute) { 
+    this.handleClickUl = this.handleClickUl.bind(this);
+    this.handleClickBtn = this.handleClickBtn.bind(this);
+    this.handleClickKeywords = this.handleClickKeywords.bind(this)
+  }
   handleClick(item) {
     //console.log(item)
     this.router.navigate(['./index/detail'], { queryParams: { '_id': item._id } });
@@ -97,48 +101,63 @@ export class ArticleComponent implements OnInit {
     this.keywords = this.route.snapshot.queryParamMap.get('keyWords')||'';
     this.loadData();
   }
-  ngAfterViewInit(){
+  handleClickUl(e){
+    console.log(this)
     this.keyWords = document.querySelector('#keyWords');
-    let btn = document.querySelector('.search');
-    btn.addEventListener('click',() => {
+    this.node = e.target;
+    // this.nodeName = this.node.nodeName;
+    if(e.target.nodeName==='LI'||e.target.nodeName==='li'){
+      console.log(window.location.pathname)
+      if(window.location.pathname!=='/index')this.router.navigate(['./index'])
+      //搜索框置空
+      this.keyWords.value='';
+      this.keywords = this.node.innerHTML;
+      if(this.keywords==='全部文章')this.keywords='';
+      this.router.navigate(['./index'],{queryParams:{keyWords:this.keywords}})
+      this.loadData(1,{ keyWords:this.keywords })
+    }
+
+  }
+  handleClickBtn(){
+    this.search = true;
+    this.router.navigate(['./index'],{queryParams:{keyWords:this.keyWords.value}})
+    this.handleClickSearch(this.keyWords.value)
+  }
+  handleClickKeywords(e){
+    if(e.keyCode===13){
       this.search = true;
       this.router.navigate(['./index'],{queryParams:{keyWords:this.keyWords.value}})
       this.handleClickSearch(this.keyWords.value)
-    })
-    this.keyWords.addEventListener('keyup',(e) => {
-      if(e.keyCode===13){
-        this.search = true;
-        this.router.navigate(['./index'],{queryParams:{keyWords:this.keyWords.value}})
-        this.handleClickSearch(this.keyWords.value)
-      }
-    })
+    }
+  }
+  ngAfterViewInit(){
+    this.keyWords = document.querySelector('#keyWords');
+    let btn = document.querySelector('.search');
+    // btn.addEventListener('click',() => {
+    //   this.search = true;
+    //   this.router.navigate(['./index'],{queryParams:{keyWords:this.keyWords.value}})
+    //   this.handleClickSearch(this.keyWords.value)
+    // })
+    btn.addEventListener('click',this.handleClickBtn,false)
+    this.keyWords.addEventListener('keyup',this.handleClickKeywords,false)
     let ul = document.getElementsByClassName('ul')[0];
-    ul.addEventListener('click',(e) => {
-      this.node = e.target;
-      this.nodeName = this.node.nodeName;
-      if(this.nodeName==='LI'||this.nodeName==='li'){
-        console.log(window.location.pathname)
-        if(window.location.pathname!=='/index')this.router.navigate(['./index'])
-        //搜索框置空
-        this.keyWords.value='';
-        this.keywords = this.node.innerHTML;
-        if(this.keywords==='全部文章')this.keywords='';
-        this.router.navigate(['./index'],{queryParams:{keyWords:this.keywords}})
-        this.loadData(1,{ keyWords:this.keywords })
-      }
-
-    })
+    ul.addEventListener('click',this.handleClickUl,false)
+    
   }
   ngOnDestroy(){
+    let ul = document.getElementsByClassName('ul')[0];
+    ul.removeEventListener('click',this.handleClickUl,false)
     let btn = document.querySelector('.search');
-    btn.removeEventListener('click',() => {
-      this.handleClickSearch(this.keyWords.value)
-    },false)
-    this.keyWords.removeEventListener('keyup',(e) => {
-      if(e.keyCode===13){
-        this.handleClickSearch(this.keyWords.value)
-      }
-    },false)
+    // btn.removeEventListener('click',() => {
+    //   this.handleClickSearch(this.keyWords.value)
+    // },false)
+    btn.removeEventListener('click',this.handleClickBtn,false)
+    // this.keyWords.removeEventListener('keyup',(e) => {
+    //   if(e.keyCode===13){
+    //     this.handleClickSearch(this.keyWords.value)
+    //   }
+    // },false)
+    this.keyWords.removeEventListener('keyup',this.handleClickKeywords,false)
   }
   async handleClickSearch(keyWords) {
     try {
