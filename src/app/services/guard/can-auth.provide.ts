fa20c2be5aa767f7a293d42eef60a333c5418d27
dEdwardy <957196958@qq.com>
2019-03-08 22:24:21 +0800

    增加前端权限判断

--- src/app/services/guard/can-auth.provide.ts
+++ src/app/services/guard/can-auth.provide.ts
@@ -1,25 +1,50 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Utils } from '../../common/helper/utils-helper'
-
+import { LoginService } from '../../services/login/login.service'
 
 @Injectable()
 export class CanAuthProvide implements CanActivate {
   constructor(
     private router: Router,
+    public loginService: LoginService
   ) { }
-
+  authority:any;
+  authorityRes:any;
   public async canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Promise<any> {
     return this.can();
   }
-
-  can() {
+  async checkAuthority() {
+    let userinfo = Utils.getCookie("userinfo");
+    if (!userinfo) {
+      this.authority = 0;
+      return;
+    }
+    let email = JSON.parse(userinfo).email;
+    console.log(JSON.parse(userinfo));
+    console.log(email);
+    try {
+      this.authorityRes = await this.loginService.uniqueEmail({ email });
+      this.authority = this.authorityRes.authority;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async can() {
     let token = Utils.getCookie('userinfo')
     if(token){
       console.log('已登录')
-      return true;
+      await this.checkAuthority();
+      if(this.authority===1){
+        console.log('admin')
+        return true;
+      }else{
+        this.router.navigate(['./index']);
+        console.log('非admin')
+        return false;
+      }
     }
     else{
       this.router.navigate(['./index']);
